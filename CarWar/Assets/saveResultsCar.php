09702cae584e66f4c9cd5c6bcaa0aabb857c3d22
Minindu Weerakoon <48473924+minindu0@users.noreply.github.com>
2022-11-04 13:21:22 -0500

    CarWar upload

---
+++ CarWar/Assets/saveResultsCar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+$con = mysqli_connect('localhost','minibndo_user','avarice','minibndo_first');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+$q = strval($_GET['q']);
+$r = strval($_GET['r']);
+
+mysqli_select_db($con,"ajax_demo");
+
+$sql = "INSERT INTO car (name,score) VALUES ('$q','$r')";
+
+$result = mysqli_query($con,$sql);
+
+mysqli_close($con);
+
+?>
+</body>
+</html>
